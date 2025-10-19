3f9e3d3e3076adc7fec27adb8e38dcffc431431b
Abdulrahman Hanafy <abdulrahman.elhanafy.dev@gmail.com>
2025-10-19 12:09:40 +0300

    Added vercel analysis

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { CartProvider } from "./context/CartContext.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
